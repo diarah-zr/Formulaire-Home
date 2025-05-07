977fa653d7c6a867570eba99dfd647f8c25e0e83
Diarah-zr <diarahtraore78@gmail.com>
2025-05-07 10:53:53 +0200

    php css

--- index.php
+++ index.php
@@ -1,14 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Connexion</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <header>
+        <h1>📜 Formulaire de Connexion</h1>
+    </header>
+    <main>
+        <figure>
+            <figcaption>
+                <h2>Tu es Développeur 💻</h2>
+            </figcaption>
+        </figure>
+        <fieldset>
+            <legend>Remplissez les champs</legend>
+            <form method="post" action="">
+                <label for="login">Login :</label>
+                <input type="text" id="login" name="login" required><br><br>
 
-<?php
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required><br><br>
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if(isset($_POST['login']) && isset($_POST['password'])){
-        $admin = isset($_POST['admin']) ? 1 : 0;
-        print "<p>Valeur à enregistrer en base : " . $admin."</p>";
+                <label>
+                    <input type="checkbox" name="admin" value="1"> Vous êtes admin
+                </label><br><br>
 
-        }else{
-            print "<p>Veuillez remplir tous les champs s'il vous plaît!</p>";
-
-        }
-    }
-?>
+                <button type="submit">Envoyer</button>
+            </form>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                if (isset($_POST['login']) && isset($_POST['password'])) {
+                    $admin = isset($_POST['admin']) ? 1 : 0;
+                    echo "<p>Valeur à enregistrer en base : " . $admin . "</p>";
+                } else {
+                    echo "<p>Veuillez remplir tous les champs s'il vous plaît!</p>";
+                }
+            }
+            ?>
+        </fieldset>
+    </main>
+</body>
+</html>
